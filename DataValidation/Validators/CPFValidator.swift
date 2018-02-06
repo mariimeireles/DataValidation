e517a93dcd9f857a895b8a8aaf6faef62a6b8f81
Mariana Meireles | Stone <mariana.meireles@STRJ1DEVM10862.local>
2018-02-06 17:38:57 -0200

    improving validate method

--- DataValidation/Validators/CPFValidator.swift
+++ DataValidation/Validators/CPFValidator.swift
@@ -21,28 +21,26 @@ class CPFValidator {
         
         let numbers = formattedString.flatMap({Int(String($0))})
         guard numbers.count == 11 && Set(numbers).count != 1 else { return false }
-        let soma1 = 11 - ( numbers[0] * 10 +
-            numbers[1] * 9 +
-            numbers[2] * 8 +
-            numbers[3] * 7 +
-            numbers[4] * 6 +
-            numbers[5] * 5 +
-            numbers[6] * 4 +
-            numbers[7] * 3 +
-            numbers[8] * 2 ) % 11
-        let dv1 = soma1 > 9 ? 0 : soma1
-        let soma2 = 11 - ( numbers[0] * 11 +
-            numbers[1] * 10 +
-            numbers[2] * 9 +
-            numbers[3] * 8 +
-            numbers[4] * 7 +
-            numbers[5] * 6 +
-            numbers[6] * 5 +
-            numbers[7] * 4 +
-            numbers[8] * 3 +
-            numbers[9] * 2 ) % 11
-        let dv2 = soma2 > 9 ? 0 : soma2
-        return dv1 == numbers[9] && dv2 == numbers[10]
+        
+        let div1 = makeCount(numbers: numbers, number: 10)
+        let div2 = makeCount(numbers: numbers, number: 11)
+        
+        return div1 == numbers[9] && div2 == numbers[10]
     }
+    
+    func makeCount(numbers: [Int], number: Int) ->  Int {
+        var index = 0
+        var result = 0
+        var plus = number
+        repeat {
+            result += numbers[index] * plus
+            index += 1
+            plus -= 1
+        } while plus >= 2
+        let sum = 11 - (result) % 11
+        let div = sum > 9 ? 0 : sum
+        return div
+    }
+
     
 }
