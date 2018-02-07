df58ff30122b66043f2f733ff91804ca4a51e6c9
Mariana Meireles | Stone <mariana.meireles@STRJ1DEVM10862.local>
2018-02-07 11:19:35 -0200

    changing email validator and email validator tests

--- EmailValidatorTests/EmailValidatorTests.swift
+++ EmailValidatorTests/EmailValidatorTests.swift
@@ -51,19 +51,18 @@ class EmailValidatorTests: XCTestCase {
     
     func test_shouldntAccept_invalidEmail() {
         let invalidsEmail = ["Abc.example.com", "A@b@c@example.com", "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com", "just\"not\"right@example.com", "this is\"not\\allowed@example.com", "this\\ still\\\"not\\\\allowed@example.com", "john..doe@example.com", "example@localhost", "john.doe@example..com", "\" \"@example.org"]
-//        "1234567890123456789012345678901234567890123456789012345678901234+x@example.com", "\"very.unusual.@.unusual.com\"@example.com"
-        for email in invalidsEmail{
-            let validation = emailValidatorUnderTest.validate(inputEmail: email)
-            XCTAssertEqual(validation, REJECTED)
-        }
+        emailLoop(invalidsEmail, status: REJECTED)
     }
     
     func test_shouldAccept_validEmail() {
         let validsEmail = ["prettyandsimple@example.com", "very.common@example.com", "disposable.style.email.with+symbol@example.com", "other.email-with-dash@example.com", "fully-qualified-domain@example.com", "user.name+tag+sorting@example.com", "x@example.com", "example-indeed@strange-example.com", "#!$%&'*+-/=?^_`{}|~@example.org", "example@s.solutions"]
-//        "\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\ \\\"very\\\".unusual\"@strange.example.com", "admin@mailserver1", "\"()<>[]:,;@\\\"!#$%&'-/=?^_`{}| ~.a\"@example.org", "user@localserver", "user@[2001:DB8::1]"
-        for email in validsEmail{
+        emailLoop(validsEmail, status: ACCEPTED)
+    }
+    
+    func emailLoop(_ emailArray: [String], status: Bool) -> (){
+        for email in emailArray {
             let validation = emailValidatorUnderTest.validate(inputEmail: email)
-            XCTAssertEqual(validation, ACCEPTED)
+            return XCTAssertEqual(validation, status)
         }
     }
     
